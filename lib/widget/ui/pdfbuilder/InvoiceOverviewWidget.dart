430afb2d087ce6ebab15b4b9549636095e8e4f1a
sayyidkhan <sayyidkhan92@hotmail.com>
2020-09-30 18:15:40 +0800

    doing housekeeping

--- lib/widget/ui/pdfbuilder/InvoiceOverviewWidget.dart
+++ lib/widget/ui/pdfbuilder/InvoiceOverviewWidget.dart
@@ -1,74 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:pdf_test/constant/pdf/crud/PdfBuilder.dart';
-import 'package:pdf_test/screen/PdfPreviewScreen.dart';
 
 class InvoiceOverviewWidget {
 
-  List<ListTile> _listArray = [];
-
-  List<ListTile> get listArray => _listArray;
-
-  InvoiceOverviewWidget(BuildContext context){
-    //add List Tile
-    _listArray.add(customListArray(context,"Pdf Sample 1","lorem ipsum lorem ipsum lorem ipsum",1));
-    _listArray.add(customListArray(context,"Pdf Sample 2","lorem ipsum lorem ipsum lorem ipsum",2));
-  }
-
-  ListTile customListArray(
-      BuildContext context,
-      String title,
-      String subtitle,
-      int pdfContent) {
-    return ListTile(
-      contentPadding: EdgeInsets.only(left: 10,top: 10,bottom: 10,right: 5),
-      title: Text(title),
-      // subtitle: Text(subtitle),
-      trailing: Wrap(
-        spacing: 2.5, // space between two icons
-        children: <IconButton>[
-          IconButton(
-              icon: const Icon(Icons.image),
-              tooltip: "Delete",
-              color: Colors.blue,
-              onPressed: () async {
-                print("view button");
-                PdfBuilder pdf = new PdfBuilder(title,pdfContent);
-                await navigateToPage(context, pdf);
-              }),
-          IconButton(
-              icon: const Icon(Icons.edit),
-              tooltip: "Edit",
-              color: Colors.blue,
-              onPressed: null),
-          IconButton(
-              icon: const Icon(Icons.delete),
-              tooltip: "Delete",
-              color: Colors.red.shade400,
-              onPressed: () {
-                print("delete button");
-              }),
-        ],
-      ),
-      onTap: () {
-
-      },
-    );
-  }
-
-  Future navigateToPage(BuildContext context,PdfBuilder pdf) async {
-    final content = await pdf.savePdf();
-    String fullPath = pdf.filePath();
-
-    Navigator.push(context, MaterialPageRoute(
-        builder: (context) =>
-            PdfPreviewScreen(
-              path: fullPath,
-            )
-    ));
-  }
-
   static Container emptyList() {
-
     const double fontSize1 = 34;
     const double fontSize2 = 16;
 
